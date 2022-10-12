ba9fe41514fdd064c4a93e9dc693f7a7c31ff074
Han Pham <hanpham@Hans-MacBook-Pro-3.local>
2022-10-11 19:56:16 -0700

    Commit

--- Program1/time_span.cpp
+++ Program1/time_span.cpp
@@ -1,35 +1,24 @@
 #include "time_span.h"
 #include <iostream>
+#include <math.h>
 
-TimeSpan::TimeSpan()
+TimeSpan::TimeSpan() : hours_(0), minutes_(0), seconds_(0)
 {
-    hours_ = 0;
-    minutes_ = 0;
-    seconds_ = 0;
 }
 
-TimeSpan::TimeSpan(int seconds)
+TimeSpan::TimeSpan(float seconds) : hours_(0), minutes_(0)
 {
-    seconds_ = seconds;
-    minutes_ = 0;
-    hours_ = 0;
-    ParseTimeSpan();
+    ParseTimeSpan(0,0,seconds);
 }
 
-TimeSpan::TimeSpan(int minutes, int seconds)
+TimeSpan::TimeSpan(float minutes, float seconds) : hours_(0)
 {
-    hours_ = 0;
-    minutes_ = minutes;
-    seconds_ = seconds;
-    ParseTimeSpan();
+    ParseTimeSpan(0, minutes, seconds);
 }
 
-TimeSpan::TimeSpan(int hours, int minutes, int seconds)
+TimeSpan::TimeSpan(float hours, float minutes, float seconds)
 {
-    hours_ = hours;
-    minutes_ = minutes;
-    seconds_ = seconds;
-    ParseTimeSpan();
+    ParseTimeSpan(hours, minutes, seconds);
 }
 
 int TimeSpan::getHours() const
@@ -62,33 +51,64 @@ void TimeSpan::setSeconds(int seconds)
     seconds_ = seconds;
 }
 
-void TimeSpan::ParseTimeSpan()
+void TimeSpan::ParseTimeSpan(float hours, float minutes, float seconds)
 {
-    int total_seconds = 0;
-    total_seconds += (hours_ * 3600);
-    total_seconds += (minutes_ * 60);
-    total_seconds += seconds_;
-    setHours(total_seconds % 60);
-    total_seconds = total_seconds % 60;
-    setMinutes(total_seconds % 60);
-    total_seconds = total_seconds % 60;
+    float total_seconds = 0;
+    total_seconds += (hours * 3600);
+    total_seconds += (minutes * 60);
+    total_seconds += seconds;
+    // cout << "total_seconds: " << total_seconds << endl;
+    if (total_seconds == 0)
+    {
+        setSeconds(0);
+        setMinutes(0);
+        setHours(0);
+    }
+    if ((total_seconds >= 3600) || (total_seconds <= -3600))
+    {
+        setHours(total_seconds / 3600);
+        total_seconds = fmod(total_seconds,3600);
+    }
+    if ((total_seconds >= 60) || (total_seconds <= -60))
+    {
+        setMinutes(total_seconds / 60);
+        total_seconds = fmod(total_seconds,60);
+    }
     setSeconds(total_seconds);
 }
 
 ostream& operator << (ostream& os, const TimeSpan ts)
 {
-    if ((ts.getHours() > 0))
+    return os << "Hours: " << ts.getHours() << ", Minutes: " << ts.getMinutes() << ", Seconds: " << ts.getSeconds();
+}
+
+TimeSpan& TimeSpan::operator=(const TimeSpan& ts)
+{
+    if (this == &ts)
     {
-        os << "Hours: " << ts.getHours() << ", Minutes: " << ts.getMinutes() << ", Seconds: " << ts.getSeconds();
-    } else if ((ts.getMinutes() > 0))
-    {
-        os << "Minutes: " << ts.getMinutes() << ", Seconds: " << ts.getSeconds();
-    } else if ((ts.getSeconds() > 0))
-    {
-        os << "Seconds: " << ts.getSeconds();
-    } else 
-    {
-        os << "0";
+        return *this;
+    } else {
+        hours_ = ts.hours_;
+        minutes_ = ts.minutes_;
+        seconds_ = ts.seconds_;
+        return *this;
     }
-    return os;
+}
+
+void TimeSpan::operator+(TimeSpan ts)
+{
+    hours_ += ts.getHours();
+    minutes_ += ts.getMinutes();
+    seconds_ += ts.getSeconds();
+    ParseTimeSpan(hours_, minutes_, seconds_);
+}
+
+void TimeSpan::operator+=(TimeSpan& ts)
+{
+
+}
+
+void TimeSpan::operator-=(TimeSpan& ts)
+{
+
 }
--- Program1/time_span.h
+++ Program1/time_span.h
@@ -8,24 +8,43 @@ class TimeSpan
 {
     public:
         TimeSpan();
-        TimeSpan(int seconds);
-        TimeSpan(int minutes, int seconds);
-        TimeSpan(int hours, int minutes, int seconds);
+        TimeSpan(float seconds);
+        TimeSpan(float minutes, float seconds);
+        TimeSpan(float hours, float minutes, float seconds);
 
         int getHours() const;
         int getMinutes() const;
         int getSeconds() const;
 
-        void ParseTimeSpan();
         void setHours(int hours);
         void setMinutes(int minutes);
         void setSeconds(int seconds);
 
+
         friend ostream& operator << (ostream& os, const TimeSpan ts);
+        
+        TimeSpan& operator=(const TimeSpan& ts);
+        //Comparison operators overload
+        bool operator==(const TimeSpan& ts) const;
+        bool operator!=(const TimeSpan& ts) const;
+        bool operator < (const TimeSpan& ts) const;
+        bool operator > (const TimeSpan& ts) const;
+        bool operator <= (const TimeSpan& ts) const;
+        bool operator >= (const TimeSpan& ts) const;
+
+        //Uniary operators overload
+        void operator + (TimeSpan st);
+        void operator - (TimeSpan& st);
+
+        //Assignment operators overload
+        void operator-=(TimeSpan& st);
+        void operator+=(TimeSpan& st);
+
         
     private:
         int hours_;
         int minutes_;
         int seconds_;
+        void ParseTimeSpan(float hours, float minutes, float seconds);
 };
 #endif
