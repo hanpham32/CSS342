ee8d24aceb8b0819c599d4cc5758acd92cfdddd3
Han Pham <hanpham@Hans-MacBook-Pro-4.local>
2022-10-15 22:09:37 -0700

    Commit

--- Program1/time_span.h
+++ Program1/time_span.h
@@ -6,46 +6,49 @@ using namespace std;
 
 class TimeSpan
 {
-    public:
-        TimeSpan();
-        TimeSpan(float seconds);
-        TimeSpan(float minutes, float seconds);
-        TimeSpan(float hours, float minutes, float seconds);
+public:
+    // Constructors
+    TimeSpan();
+    TimeSpan(double seconds);
+    TimeSpan(double minutes, double seconds);
+    TimeSpan(double hours, double minutes, double seconds);
 
-        int getHours() const;
-        int getMinutes() const;
-        int getSeconds() const;
+    // Accessors
+    int hours() const;
+    int minutes() const;
+    int seconds() const;
 
-        friend ostream& operator << (ostream& os, const TimeSpan ts);
-        friend ostream& operator >> (istream& is, TimeSpan& ts);
-        
-        //Unary operators overload
-        TimeSpan operator + (TimeSpan& st) const;
-        TimeSpan operator - (TimeSpan& st) const;
+    friend ostream &operator<<(ostream &os, const TimeSpan ts);
+    friend istream &operator>>(istream &is, TimeSpan &ts);
 
-        //Relational operators overload
-        bool operator==(const TimeSpan& ts) const;
-        bool operator!=(const TimeSpan& ts) const;
-        bool operator < (const TimeSpan& ts) const;
-        bool operator > (const TimeSpan& ts) const;
-        bool operator <= (const TimeSpan& ts) const;
-        bool operator >= (const TimeSpan& ts) const;
+    // Unary operators overload
+    TimeSpan operator+(TimeSpan &st);
+    TimeSpan operator-(TimeSpan &st);
 
-        //Assignment operators overload
-        void operator=(const TimeSpan& ts);
+    // Relational operators overload
+    bool operator==(const TimeSpan &ts) const;
+    bool operator!=(const TimeSpan &ts) const;
+    bool operator<(const TimeSpan &ts) const;
+    bool operator>(const TimeSpan &ts) const;
+    bool operator<=(const TimeSpan &ts) const;
+    bool operator>=(const TimeSpan &ts) const;
 
-        TimeSpan& operator-=(const TimeSpan& st); // return reference object
-        TimeSpan& operator+=(const TimeSpan& st);
+    // Assignment operators overload
+    void operator=(const TimeSpan &ts);
 
-        
-    private:
-        int hours_;
-        int minutes_;
-        int seconds_;
-        void ParseTimeSpan(float hours, float minutes, float seconds);
-        float CalculateTotalSeconds(float hours, float minutes, float seconds);
-        void setHours(int hours);
-        void setMinutes(int minutes);
-        void setSeconds(int seconds);
+    TimeSpan operator-=(const TimeSpan &st);
+    TimeSpan operator+=(const TimeSpan &st);
+
+    void set_time(int hours, int minutes, int seconds);
+    double CalculateTotalSeconds(double hours, double minutes, double seconds);
+    void ParseTimeSpan(TimeSpan &ts, int total_seconds);
+
+private:
+    int hours_;
+    int minutes_;
+    int seconds_;
+    void set_hours(int hours);
+    void set_minutes(int minutes);
+    void set_seconds(int seconds);
 };
 #endif
