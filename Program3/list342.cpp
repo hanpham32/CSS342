d7a193f1fb4b573bafd96533e1dd2e58d6bf6f4b
Han Pham <hanpham@Hans-MacBook-Pro-4.local>
2022-11-04 19:24:31 -0700

    commit

--- Program3/list342.cpp
+++ Program3/list342.cpp
@@ -4,32 +4,52 @@
 #include <fstream>
 using namespace std;
 
+int main()
+{
+    List342<string> list;
+    cout << "hello world" << endl;
+    return 0;
+}
+
 template <class T>
 List342<T>::List342() : head_(nullptr)
 {
 }
 
-template <class T>
-List342<T>::List342(const List342 &source)
-{
-}
+// template <class T>
+// List342<T>::List342(const List342 &source)
+// {
+// }
 
-template <class T>
-List342<T> &List342<T>::operator=(const List342<T> &source)
-{
-}
+// template <class T>
+// List342<T> &List342<T>::operator=(const List342<T> &source)
+// {
+// }
 
 /// @brief open and read objects from a file and build an ordered list
 /// no duplicate item allowed
 /// @tparam T
 /// @param file_name
-/// @return
+/// @return true if read successfully
 template <class T>
 bool List342<T>::BuildList(string file_name)
 {
+    // fstream newfile;
+    // newfile.open(file_name, ios::in);
+    // if (newfile.is_open())
+    // {
+    //     string tp;
+    //     while (getline(newfile, tp))
+    //     {
+    //         cout << tp << " ";
+    //     }
+    //     newfile.close();
+    //     return true;
+    // }
+    // return false;
 }
 
-template <class T>
-ostream &operator<<(ostream &stream, List342<T> &list)
-{
-}
+// template <class T>
+// ostream &operator<<(ostream &stream, List342<T> &list)
+// {
+// }
---
+++ Program3/test_temp/test.cpp
@@ -0,0 +1,18 @@
+#include "test.h"
+using namespace std;
+
+template <class T>
+Test<T>::Test()
+{
+}
+
+template <class T>
+void Test<T>::set_value(T obj_i)
+{
+}
+
+template <class T>
+T Test<T>::get_value()
+{
+    return m_obj_;
+}
