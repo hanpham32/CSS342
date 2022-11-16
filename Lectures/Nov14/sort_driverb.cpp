acd661bcd492c56f6ad783a0abe0deb36d2f641d
Han Pham <hanpham@Hans-MacBook-Pro-4.local>
2022-11-15 17:33:29 -0800

    Commit

---
+++ Lectures/Nov14/sort_driverb.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+
+void MergeSort(std::vector<int> &vec, int first, int last);
+void Merge(std::vector<int> &vec, int first, int mid, int last);
+
+int main()
+{
+    return 0;
+}
+
+void Merge(std::vector<int> &vec, int first, int mid, int last)
+{
+    int size = last - first + 1;
+    int *tmp_arr; // dynamic => a pointer is needed
+    tmp_arr = new int[size];
+
+    // set indexes for beginning and end of each subarray
+    // how many indexes are in this program?
+    //// ans = 5
+    int first1 = first, last1 = mid;
+    int first2 = mid + 1, last2 = last;
+    int index = 0;
+
+    while ((first1 <= last1) && (first2 <= last2))
+    {
+        if (vec[first1] < vec[first2])
+        {
+            tmp_arr[index] = vec[first1];
+            first1++;
+        }
+        else
+        {
+            tmp_arr[index] = vec[first2];
+            first2++;
+        }
+        index++;
+    }
+
+    while (first1 <= last1)
+    {
+        tmp_arr[index] = vec[first1];
+        first1++;
+        index++;
+    }
+    while (first2 <= last2)
+    {
+        tmp_arr[index] = vec[first2];
+        first2++;
+        index++;
+    }
+
+    for (index = 0; index < size; index++)
+    {
+        vec[first] = tmp_arr[index];
+        first++;
+    }
+    delete[] tmp_arr;
+}
+
+void MergeSort(std::vector<int> &vec, int first, int last)
+{
+    if (first < last)
+    {
+        int mid = (first + last) / 2;
+        MergeSort(vec, first, mid);
+        MergeSort(vec, mid + 1, last);
+        Merge(vec, first, mid, last);
+    }
+}
