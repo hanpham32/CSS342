3fecd373c8f0d8ebc510855a0a8755d328866990
Han Pham <hanpham@Hans-MacBook-Pro-4.local>
2022-12-07 14:35:44 -0800

    Program 5

---
+++ Program5/client.h
@@ -0,0 +1,9 @@
+#ifndef CLIENT_H_
+#define CLIENT_H_
+
+class Client
+{
+public:
+private:
+};
+#endif
---
+++ Program5/clients.h
@@ -0,0 +1,8 @@
+/*
+FileName: clients.h
+Created On: November 27th, 2022
+Author: Hannah Pham
+Course: CSS 342 B Fall 22
+Instructor: Robert Dimpsey
+Description: Program 5. A file to store all clients
+*/
