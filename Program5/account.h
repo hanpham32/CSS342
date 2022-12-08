852221faf8ffe6d68be3fdbcb98d4658fdbb3ce1
hanpham32 <yuniroiep@gmail.com>
2022-12-08 13:54:06 -0800

    Program5

--- Program5/account.h
+++ Program5/account.h
@@ -6,12 +6,23 @@ Author: Hannah Pham
 Course: CSS 342 B Fall 22
 Instructor: Robert Dimpsey
 */
+#include <iostream>
+#include <string>
+
 #ifndef ACCOUNT_H_
 #define ACCOUNT_H_
 
 class Account
 {
 public:
+    friend std::ostream &operator<<(std::ostream &os, const Account &account)
+    {
+        os << account.id_ << " " << account.last_name_ << " " << account.first_name_ << std::endl;
+    }
+
 private:
+    int id_;
+    std::string first_name_;
+    std::string last_name_;
 };
 #endif
