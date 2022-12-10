6360e0b4d5b9f9579c0fcf314d0217db6e260c92
hanpham32 <yuniroiep@gmail.com>
2022-12-09 21:33:54 -0800

    Program5

--- Program5/transaction_queue.h
+++ Program5/transaction_queue.h
@@ -5,55 +5,29 @@
 #ifndef TRANSACTION_QUEUE_H_
 #define TRANSACTION_QUEUE_H_
 
-class Node
-{
-public:
-    Node(Transaction data) : data_(data), next_(nullptr) {}
-    Transaction data_;
-    Node *next_;
-};
+// class Node
+// {
+// public:
+//     Node(Transaction data) : data_(data), next_(nullptr) {}
+//     Transaction data_;
+//     Node *next_;
+// };
 
 class TransactionQueue
 {
 private:
-    Node *front_;
-    Node *back_;
+    // Node *front_;
+    // Node *back_;
     int count_ = 0;
 
 public:
-    TransactionQueue() : front_(nullptr), back_(nullptr) {}
-    ~TransactionQueue()
-    {
-        Node *curr = front_;
-        while (curr)
-        {
-            Node *next = curr->next_;
-            delete curr;
-            curr = next;
-        }
-    }
-
     void Push(Transaction data)
     {
-        Node *ins_node = new Node(data);
-
-        if (!front_)
-        {
-            front_ = back_ = ins_node;
-        }
-        else
-        {
-            back_->next_ = ins_node;
-            back_ = ins_node;
-        }
     }
 
-    void Pop() throw(exception)
+    // CANNOT THROW EXCEPTION?
+    void Pop()
     {
-        if (!front_)
-        {
-            throw exception("Queue is empty");
-        }
     }
 
     bool isEmpty() const
